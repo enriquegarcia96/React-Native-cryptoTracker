9b3c21633f61f4b8182c84cd1a2bcdf713d777d2
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-01 11:05:53 -0600

    se modifico el diseño de la pantalla principal

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,7 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, FlatList,Text,ActivityIndicator, Pressable, StyleSheet} from 'react-native';
+import  Http from '../../libs/http';
+import CoinsItem from './CoinsItem';
+import Colors from '../../res/colors';
 
 class CoinsScreen extends Component {
+
+  state = {
+    coins: [],
+    loading: false
+  }
+
+  componentDidMount  = async () =>{
+
+    this.setState({ loading: true })
+
+    //--- recibo lo que la API me trajo --//
+    const res = await Http.instance.get("https://api.coinlore.net/api/tickers/");
+      //console.log("coins: ",coins);
+
+    //--- se lo seteo al estado ---//
+    this.setState({ coins: res.data, loading: false })
+  }
 
   handlePress = () =>{
     console.log('go to detail', this.props);
@@ -11,12 +31,22 @@ class CoinsScreen extends Component {
   }
 
   render() {
+    const { coins, loading } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text style={styles.titleText}>Coins Screen</Text>
-        <Pressable style={styles.btn} onPress={this.handlePress}>
-          <Text style={styles.btnText}>Ir a detail</Text>
-        </Pressable>
+        { loading ? 
+        <ActivityIndicator 
+          style={ styles.loader }      
+          color='blanck' 
+          size='large' 
+        />
+          : null
+        }
+        <FlatList
+          data={ coins }
+            renderItem={({ item }) => <CoinsItem item={item} />}
+         />
       </View>
     );
   }
@@ -27,7 +57,7 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: Colors.charade,
     
   },
   titleText: {
@@ -43,6 +73,9 @@ const styles = StyleSheet.create({
   btnText: {
     color: '#fff',
     textAlign: 'center'
+  },
+  loader:{
+    marginTop: 60
   }
 });
 
