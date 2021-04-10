d9e26e6060c52e90d2de0e9d9a386be44ab88ea8
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-09 22:47:04 -0600

    configuracion del archivo para las letras

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+//--- Aqui puedo personalizar mis fuentes de letras para la APP ---//
+
+module.exports = {
+    project: {
+        ios:{},
+        android:{},
+    },
+    assets: ['./src/assets/fonts']
+}
